2273f3482ecaa9975c3413dc32501df607760464
Ben <benjaminfkile@gmail.com>
2026-04-09 20:36:24 -0500

    fix .env variable

--- src/lib/cognitoClient.ts
+++ src/lib/cognitoClient.ts
@@ -8,7 +8,7 @@ import {
 
 const userPool = new CognitoUserPool({
   UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID!,
-  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID!,
+  ClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID!,
 });
 
 /** Register a new user. Does NOT sign them in — they must confirm first. */
