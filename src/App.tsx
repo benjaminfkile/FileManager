009ca1eaf2ced1f7546e30ef7d761468e4801626
Ben Kile <benjaminfkile@gmail.com>
2026-04-09 19:11:31 -0500

    fix: complete issue #44 - Add /login route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import AppShell from './components/AppShell';
 import InterceptorSetup from './components/InterceptorSetup';
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DrivePage from './pages/DrivePage';
 import FolderPage from './pages/FolderPage';
@@ -14,6 +15,7 @@ function App() {
     <>
     <InterceptorSetup />
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route element={<ProtectedRoute />}>
         <Route element={<AppShell />}>
